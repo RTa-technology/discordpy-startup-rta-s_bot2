cb787ebbdf74f8b02595059fa7def90f6cfa2fb6
RTa-technology <57354947+RTa-technology@users.noreply.github.com>
2020-03-05 20:05:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,7 +6,7 @@ from cog_basic import Basic
 from cog_greet import Greet
 
 
-bot = rta.Bot(command_prefix='$', help_command=JapaneseHelpCommand())
+bot = rta.Bot(command_prefix='$')#, help_command=JapaneseHelpCommand()
 
 token = os.environ['DISCORD_BOT_TOKEN']
 
@@ -23,17 +23,17 @@ async def ping(ctx):
 
 
 
-class JapaneseHelpCommand(commands.DefaultHelpCommand):
-    def __init__(self):
-        super().__init__()
-        self.commands_heading = "コマンド:"
-        self.no_category = "その他"
-        self.command_attrs["help"] = "コマンド一覧と簡単な説明を表示"
-
-    def get_ending_note(self):
-        return (f"各コマンドの説明: $help <コマンド名>\n"
-                f"各カテゴリの説明: $help <カテゴリ名>\n")
-
+#class JapaneseHelpCommand(commands.DefaultHelpCommand):
+#   def __init__(self):
+#        super().__init__()
+#        self.commands_heading = "コマンド:"
+#        self.no_category = "その他"
+#        self.command_attrs["help"] = "コマンド一覧と簡単な説明を表示"
+#
+#    def get_ending_note(self):
+#        return (f"各コマンドの説明: $help <コマンド名>\n"
+#                f"各カテゴリの説明: $help <カテゴリ名>\n")
+#
 
 bot.add_cog(Greet(bot=bot))
 bot.add_cog(Basic(bot=bot))
