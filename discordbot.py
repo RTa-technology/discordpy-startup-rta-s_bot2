42aa199a0200350cd0c3c48b3e01594cc8ad567b
RTa-technology <57354947+RTa-technology@users.noreply.github.com>
2020-03-05 19:05:22 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,8 +1,9 @@
 from discord.ext import commands
 import os
 import traceback
+prefix = '$'
 
-bot = commands.Bot(command_prefix='$',help_command=JapaneseHelpCommand())
+bot = commands.Bot(command_prefix=prefix, help_command=JapaneseHelpCommand())
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
@@ -12,35 +13,40 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-class basic(bot.Cog, name='基本動作'):
+@bot.command(name="ping")
+async def ping(ctx):
+        await ctx.send('pong')
+
+
+class basic(commands.Cog, name='基本動作'):
     def __init__(self, bot):
         super().__init__()
         self.bot = bot
     
-    @bot.command(name="ping")
-    async def ping(self,ctx):
-        await ctx.send(f"{ctx.author.name}\npong")
+    @commands.command(name="pin")
+    async def pin(self,ctx):
+        await ctx.send(f"pon {ctx.author.name}")
 
-class Greet(bot.Cog, name='Greeting'):
+class Greet(commands.Cog, name='あいさつ'):
     def __init__(self, bot):
         super().__init__()
         self.bot = bot
 
-    @bot.command(name="hello")
+    @commands.command(name="こんにちは")
     async def hello(self, ctx):
         """出会いのあいさつをする"""
         await ctx.send(f"どうも、{ctx.author.name}さん!")
 
-    @bot.command(name="goodbye")
+    @commands.command(name="さようなら")
     async def goodbye(self, ctx):
         """別れの挨拶をする"""
         await ctx.send(f"じゃあね、{ctx.author.name}さん!")
 
 
-class JapaneseHelpCommand(bot.DefaultHelpCommand):
+class JapaneseHelpCommand(commands.DefaultHelpCommand):
     def __init__(self):
         super().__init__()
-        self.bot_heading = "コマンド:"
+        self.commands_heading = "コマンド:"
         self.no_category = "その他"
         self.command_attrs["help"] = "コマンド一覧と簡単な説明を表示"
 
@@ -49,6 +55,7 @@ class JapaneseHelpCommand(bot.DefaultHelpCommand):
                 f"各カテゴリの説明: {prefix}help <カテゴリ名>\n")
 
 
+bot = commands.Bot(command_prefix=prefix, help_command=JapaneseHelpCommand())
 bot.add_cog(Greet(bot=bot))
 bot.add_cog(basic(bot=bot))
 bot.run(token)
