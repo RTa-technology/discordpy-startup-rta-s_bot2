d1b48d4659daf834b54633b1e4d1add0011dd974
RTa-technology <57354947+RTa-technology@users.noreply.github.com>
2020-03-06 00:56:18 +0900

    Create cog_wikidis.py

---
+++ cog_wikidis.py
@@ -0,0 +1,24 @@
+from discord.ext import commands as rta
+class WikiDiscord(rta.Cog, name="Wd_to_discord"):
+    def __init__(self, bot):
+        super().__init__()
+        self.bot = bot
+        
+    @rta.event
+    async def on_message(message):
+        """Wikidot Markdown → Discord Markdown"""
+        if message.content.startswith('/ds'):
+        # 文字から「/ds」を抜く
+        dis_md = message.content[len('/ds'):].strip()
+                
+        discord1_md = dis_md.replace('//', '*').replace('**', '**').replace('//**', '***').replace('**//', '***').replace('__', '__').replace('--', '~~')
+
+            if re.match('##[a-z]+|', discord1_md):
+                discord2_md = discord1_md.replace('##red|', '```autohotkey\n').replace('##blue|','```ini\n[').replace('##yellow|','```md\n< ').replace('##orange|','```py\n@ ').replace('##green|','```diff\n+ ').replace('##cyan|','```py\n# ')
+                discord3_md = discord2_md.replace('##','\n```')
+            else:
+                discord3_md = discord1_md
+
+            discord_md = discord3_md.replace('[[code]]','```\n').replace('[[/code]]','\n```')
+                
+        await message.channel.send(discord_md)
