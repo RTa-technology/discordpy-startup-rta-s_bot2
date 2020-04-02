e4e2f16304db66c7bd33e1367c488b6533d3c44d
RTa-technology <57354947+RTa-technology@users.noreply.github.com>
2020-04-03 02:55:44 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -19,18 +19,13 @@ async def on_command_error(ctx, error):
 @bot.command(name="ping")
 async def ping(ctx):
         await ctx.send('pong')
-
+        
 @bot.command()
-async def dice(ctx, dice: str):
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await ctx.send('Format has to be in NdN!')
-        return
+async def joined(member : discord.Member):
+    await bot.say('{0.name} joined in {0.joined_at}'.format(member))
 
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    msg = f"{ctx.author.mention}\n" + result 
-    await ctx.send(msg)
+
+
 
 #class JapaneseHelpCommand(commands.DefaultHelpCommand):
 #   def __init__(self):
