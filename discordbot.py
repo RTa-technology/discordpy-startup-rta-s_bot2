aaaf5713dbbefebb171afda9113e10e952fa28c9
RTa-technology <57354947+RTa-technology@users.noreply.github.com>
2020-04-03 03:40:55 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -20,11 +20,17 @@ async def on_command_error(ctx, error):
 @bot.command(name="ping")
 async def ping(ctx):
         await ctx.send('pong')
-        
-@bot.command()
-async def joined(member : discord.Member):
-    await bot.say('{0.name} joined in {0.joined_at}'.format(member))
 
+@bot.command(name="dice")
+async def dice(ctx, dice: str):
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    msg = f"{ctx.author.mention}\n" + result 
+    await ctx.send(msg)
 
 
 
